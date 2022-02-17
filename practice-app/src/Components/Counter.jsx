ed988d01f5e63e05837e3aaabca42ec5e38c186e
machineawin <machineawin@gmail.com>
2022-02-17 22:49:09 +1100

    Create counter app with increment button

--- practice-app/src/Components/Counter.jsx
+++ practice-app/src/Components/Counter.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Counter extends Component {
-    state = {  } 
+    state = { 
+        value: 0
+     } 
+
+    countFormat () {
+        if (this.state.value == 0)
+        return (<span className='badge bg-dark'> Zero </span>);
+        return (<span className='badge bg-secondary'> {this.state.value} </span>);
+
+    };
+
+    handleIncrement = () => {
+        const value = this.state.value +1;
+        this.setState ({value});
+    }
+
     render() { 
         return (
             <React.Fragment>
-                <button className="m-2 badge-primary ">Increment</button>
+                <button 
+                className="btn btn-primary m-2"
+                onClick={ this.handleIncrement }
+                >
+                    Increment
+                </button>
+                {this.countFormat()}
                 
             </React.Fragment>
         );
