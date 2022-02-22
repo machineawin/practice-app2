6d4063dda5d9dd0c50705747cc32ebf114894882
machineawin <machineawin@gmail.com>
2022-02-22 21:00:16 +1100

    Temporary pause commit

--- practice-app/src/Components/Counter.jsx
+++ practice-app/src/Components/Counter.jsx
@@ -4,32 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Counter extends Component {
          
     countFormat () {
-        if (this.props.value == 0)
+        if (this.props.counter.value == 0)
         return (<span className='badge bg-dark'> Zero </span>);
-        return (<span className='badge bg-secondary'> {this.props.value} </span>);
+        return (<span className='badge bg-secondary'> {this.props.counter.value} </span>);
 
     };
 
-    handleIncrement = () => {
-        const value = this.props.value +1;
-        this.setState ({value});
-    }
 
     render() { 
-
-        console.log ('id', this.props.id);
-        console.log ('value', this.props.value);
-
-
+        
         return (
             <div>
+                <h4>Counter #{this.props.counter.id}</h4>
                 <button 
                 className="btn btn-primary m-2"
-                onClick={ this.handleIncrement }
-                >
+                onClick={ () => this.props.handleIncrement(this.props.counter) }>
                     Increment
                 </button>
                 {this.countFormat()}
+                
+                <button
+                onClick={() => this.props.handleDelete(this.props.counter.id)}
+                className="btn btn-danger m-2">
+                    Delete
+                </button>
                 
             </div>
         );
--- practice-app/src/Components/Counters.jsx
+++ practice-app/src/Components/Counters.jsx
@@ -4,20 +4,56 @@ import React, { Component } from 'react';
 class Counters extends Component {
     state = { 
         counters: [
-            {id:1, value:3},
+            {id:1, value:0},
             {id:2, value:4},
             {id:3, value:6},
             {id:4, value:5}
         ]
-     }; 
+    }; 
+    
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];        
+        const index = counters.indexOf(counter);         
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState ({counters});
+    };
+
+    handleDelete = (counterID) => {
+        console.log ('handleDelete Clicked');
+        const counters = this.state.counters.filter (counter => (counter.id !== counterID) );
+        this.setState ({counters});
+    }
+10
+    handleReset = () => {
+        const counters = this.state.counters.map (counter => {
+            counter.value = 0
+            return counter
+         });
+        console.log ('counters after reset ', counters)
+        this.setState ({counters});
+    }
+    
     render() { 
         return (
             <React.Fragment>
-                {this.state.counters.map (c => <Counter key={c.id} id = {c.id} value = {c.value} />)}
+                <button 
+                onClick={this.handleReset}
+                className="btn btn-primary m-2">
+                    Reset
+                </button>
+
+                {this.state.counters.map (counter =>
+                     <Counter 
+                     key = {counter.id}
+                     counter={counter} 
+                     handleIncrement = {this.handleIncrement}
+                     handleDelete = {this.handleDelete}                     
+                     />)}
             </React.Fragment>
             
         );
-    }
+    };
 }
  
 export default Counters;
